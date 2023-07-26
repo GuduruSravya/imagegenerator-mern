4ef3c0a70fd3364f74f3faec992b9af38a170683
Sravya <gudursravya@gmail.com>
2023-07-26 13:43:16 -0500

    Added required routes

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,17 @@ import express from 'express';
 import * as dotenv from 'dotenv'
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
+import postRoutes from './mongodb/routes/postRoutes.js';
+import aiRoutes from './mongodb/routes/aiRoutes.js';
+
 
 dotenv.config()
 
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use('/api/v1/post',postRoutes);
+app.use('/api/v1/ai',aiRoutes);
 
 
 
