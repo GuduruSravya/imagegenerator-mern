2444b5af358cca365eb100f5455aea1a7c634585
Sravya <gudursravya@gmail.com>
2023-07-27 12:33:54 -0500

    get/post from cloudinary is done

--- backend/mongodb/routes/postRoutes.js
+++ backend/mongodb/routes/postRoutes.js
@@ -8,5 +8,45 @@ dotenv.config();
 const router = express.Router();
 
 
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD,
+    api_key:process.env.CLOUDINARY_APIKEY,
+    api_secret:process.env.CLOUDINARY_APISECRET
+
+})
+
+
+//Get posts
+router.get('/',async(req,res)=>{
+    try {
+        const post = await Post.find({});
+
+        res.status(200).json({success:true,data:post});
+    } catch (error) {
+        res.status(500).json({success:false,message:error});
+        
+    }
+})
+
+//upload posts
+router.post('/',async(req,res)=>{
+    try {
+        const {name,prompt,photo} = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+        name,
+        prompt,
+        photo:photoUrl.url,
+    })
+
+    res.status(201).json({success:true, data:newPost})
+        
+    } catch (error) {
+        res.status(500).json({success:false,message:error});
+    }
+})
+
+
 
 export default router;
